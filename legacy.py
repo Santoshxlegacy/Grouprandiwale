fc63da423f36ddc11b5837025484cba1d26969d6
Santoshxlegacy <santoshxlegacy@outlook.com>
2025-03-23 03:04:20 +0530

    Update legacy.py

--- legacy.py
+++ legacy.py
@@ -756,7 +756,7 @@ async def run_attack(ip, port, time_duration, update, user_id):
         await update.message.reply_text(f"⚠️ *Attack Error:* {str(e)}", parse_mode="Markdown")
     
 # Default thread value
-default_thread = "900"
+default_thread = "998"
 
 # Command to set thread dynamically
 async def set_thread(update: Update, context: ContextTypes.DEFAULT_TYPE):
